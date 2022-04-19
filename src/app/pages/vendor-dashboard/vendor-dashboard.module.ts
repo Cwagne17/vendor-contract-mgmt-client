7cff8b83190f7f76871be447d2067840731a48e8
Leonardo Conroy <leeleonard936@gmail.com>
2022-04-19 17:45:37 -0400

    Recommit

--- src/app/pages/vendor-dashboard/vendor-dashboard.module.ts
+++ src/app/pages/vendor-dashboard/vendor-dashboard.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VendorDashboardComponent } from './vendor-dashboard.component';
 import { VendorFilterComponent } from './components/vendor-filter/vendor-filter.component';
@@ -30,6 +30,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
     RouterModule.forChild([
       {
         path: '',
@@ -39,6 +41,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   ],
   exports: [
     RouterModule
-  ]
+  ],
+  schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
 })
 export class VendorDashboardModule { }
