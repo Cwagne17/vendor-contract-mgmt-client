f1c22061262326ddddc9ba1ce7ea5a0d25b2c346
bmurr1 <bryan42301@gmail.com>
2022-05-07 15:43:13 -0400

    Merge branch 'Leo_Conroy' of https://github.com/Cwagne17/vendor-contract-mgmt-client into venForm

--- src/app/pages/contract-dashboard/contract-dashboard.module.ts
+++ src/app/pages/contract-dashboard/contract-dashboard.module.ts
@@ -3,12 +3,36 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ContractDashboardComponent } from './contract-dashboard.component';
 
+//******************************************* */
+//taken from vendor-dashboard.module.ts
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDialogModule} from '@angular/material/dialog';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule} from '@angular/material/table';
+import { SharedModule } from '../../shared/shared.module';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { ContractFormComponent } from './components/contract-form/contract-form.component';
+//********************************************** */
+
 @NgModule({
   declarations: [
-    ContractDashboardComponent
+    ContractDashboardComponent,
+    ContractFormComponent
   ],
   imports: [
     CommonModule,
+    MatFormFieldModule,
     RouterModule.forChild([
       {
         path: '',
