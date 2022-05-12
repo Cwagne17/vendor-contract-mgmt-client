60a0d10dd2d8d73ac72d9c1424e01c6c361a3eeb
Cwagne17 <cwagne17@students.towson.edu>
2022-05-12 09:27:15 -0400

    pdf basically done

--- src/app/services/contract.service.ts
+++ src/app/services/contract.service.ts
@@ -8,6 +8,8 @@ import { IContractService } from './interfaces/icontract.service';
 import { SnackbarService } from './snackbar.service';
 import * as pdfMake from "pdfmake/build/pdfmake";
 import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import { TableCell, TDocumentDefinitions } from 'pdfmake/interfaces';
+import { PaymentInfo } from '../types/payment-info';
 
 (<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
@@ -110,16 +112,156 @@ export class ContractService implements IContractService {
   }
 
   downloadContract(contract: Contract): void {
-    const docDefinition = {
+    const tableBody: TableCell[][] = [
+      [ // table headers
+        { text: 'Date', style: 'tableHdr' },
+        { text: 'Check Number', style: 'tableHdr' },
+        { text: 'Amount', style: 'tableHdr' }
+      ]
+    ]
+    contract.paymentInfo.forEach( (paymentInfo: PaymentInfo) => {
+      tableBody.push([`${paymentInfo.current_date}`, `${paymentInfo.check_number}`, `${paymentInfo.amount}`]);
+    });
+
+    const docDefinition: TDocumentDefinitions = {
       content: [
-        { text: 'test'}
+        { columns: [
+            { // Vendor Name
+              text: `${contract.vendor.vendor_name}`,
+              style: ['subtitle']
+            },  
+            { // Contract Date, Contract End Date
+              text: `${contract.contract_date} - ${contract.contract_end_date}`,
+              style: ['subtitle']
+            }
+          ]
+        },
+        { // contract amount
+          text: `Contract Amount: $${contract.amount}`
+        },
+        { // WorkType
+          text: [
+            `Work Type: `,
+            {
+              text: `${contract.workType.type}`,
+              style: ['inline']
+            }
+          ]
+        },
+        { // Contact Information Section
+          stack: [
+            {
+              text: 'Contact information:',
+              style: ['h1', 'marginTop'],
+            },
+            {
+              text: [
+                'phone number: ', 
+                { 
+                  text: `${contract.vendor.contact_phone_number}\n`,
+                  style: 'inline'
+                },
+                'email address: ',
+                {
+                  text: `${contract.vendor.contact_email}\n`,
+                  style: 'inline'
+                }
+              ],
+            },
+          ]
+        },
+        {
+          stack: [
+            { 
+              text: 'Memo:\n',
+              style: ['h1', 'marginTop']
+            },
+            {
+              table: { //Contract memo
+                headerRows: 0,
+                widths: ["*"],
+                body: [
+                  [
+                    { 
+                      text: `${contract.memo}`, 
+                      style: ['inline', 'textBox']
+                    }
+                  ]
+                ]
+              }
+            }
+          ]
+        },
+        {
+          stack: [ 
+            { 
+              text: 'Conditions:\n',
+              style: ['h1', 'marginTop']
+            },
+            {
+              table: { //Contract condition
+                headerRows: 0,
+                widths: ["*"],
+                body: [
+                  [
+                    { 
+                      text: `${contract.condition}`, 
+                      style: ['inline', 'textBox']
+                    }
+                  ]
+                ]
+              }
+            }
+          ]
+        },
+        {
+          stack: [
+            {
+              text: 'Payment Information:\n',
+              style: ['h1', 'marginTop']
+            },
+            { // Table of payment info
+              table: {
+                headerRows: 1,
+                widths: ["*", "*", "*"],
+                body: tableBody
+              }
+            }
+          ]
+        }
       ],
       styles: {
-
+        marginTop: {
+          margin: [0, 20, 0, 0]
+        },
+        marginBottom: {
+          margin: [0, 0, 0, 20]
+        },
+        title: {
+          fontSize: 22,
+          bold: true
+        },
+        subtitle: {
+          fontSize: 20,
+          italics: true,
+        },
+        h1: {
+          fontSize: 16
+        },
+        inline: {
+          italics: true
+        },
+        tableHdr: {
+          bold: true
+        },
+        textBox: {
+          margin: 5
+        }
+      },
+      defaultStyle: {
+        color: '#222222'
       }
     };
-    pdfMake.createPdf(docDefinition).download();
+    pdfMake.createPdf(docDefinition).download(`${contract.contract_date}-${contract.vendor.vendor_name}.pdf`);
   }
-
-
 }
